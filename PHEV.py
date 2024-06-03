6ade64cdec5953391544033fa7925e9d09089203
alexanderlunova <alexander.lunova@rwth-aachen.de>
2024-06-03 20:48:05 +0200

    implemented experiment, efficiency etc.

--- PHEV.py
+++ PHEV.py
@@ -1,68 +1,127 @@
+import numpy as np
+
 class Car:
-    def __init__(self, battery_size=75, fuel_tank_size=50, SOC=80, ice_consumption_per_100km=15,
-                 SOC_limit = 0.2, max_el_power = 100, max_ice_power = 100, tank_level = 30,
-                 charging_interval = 24, recharge = True, charging_limit = 0.9):
+    def __init__(self, battery_size=75, fuel_tank_size=50, soc=80, ice_consumption_per_100km=15,
+                 soc_discharge_limit=0.2, max_el_power=100, max_ice_power=100, tank_level=30,
+                 charging_interval=24, recharge=True, charging_limit=0.9, c_rate=0.5,
+                 el_charging_efficiency=0.98, el_discharging_efficiency=0.9, delta_t=0.25, simulation_years=15):
 
         self.battery_size = battery_size
         self.fuel_tank_size = fuel_tank_size
-        self.SOC = SOC
+        self.SOC = soc
         self.tank_level = tank_level
         self.ice_consumption_per_100km = ice_consumption_per_100km
-        self.SOC_limit = SOC_limit
+        self.SOC_discharge_limit = soc_discharge_limit
         self.max_el_power = max_el_power
         self.max_ice_power = max_ice_power
-        self.el_consumption_ts = []
-        self.ice_consumption_ts = []
+        self.el_consumption_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
+        self.ice_consumption_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
+        self.charging_consumption_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
+        self.capacity_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
+        self.el_power_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
+        self.ice_power_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
+        self.SOC_ts = np.zeros(int(simulation_years) * 8760 * int(1/delta_t))
         self.charging_interval = charging_interval
         self.recharge = recharge
         self.charging_limit = charging_limit
+        self.c_rate = c_rate
+        self.charging_efficiency = el_charging_efficiency
+        self.discharge_efficiency = el_discharging_efficiency
+        self.charging_cycles = 0
 
+    def calc_consumption(self, consumption, distance, delta_t, time):
 
-
-
-    def calc_consumption(self, consumption, distance, delta_t):
-
-        avg_power = consumption/delta_t
+        # Calculate average power out of driving profile
+        avg_power = consumption/delta_t / self.discharge_efficiency
         el_power = avg_power
         ice_power = 0
         ice_power_share = 0
 
-        if (self.SOC < self.SOC_limit):
+        # logic to determine which motor handles the power
+
+        # if soc is smaller than soc limit, ice share is 100 %
+        if self.SOC <= self.SOC_discharge_limit:
+
             ice_power = self.max_ice_power
             ice_power_share = 1
             el_power = 0
-        elif (avg_power > self.max_el_power):
+
+        # if el motor power is not sufficient, both motors share the power
+        elif avg_power > self.max_el_power:
+
             ice_power = min(avg_power - self.max_el_power, self.max_ice_power)
             ice_power_share = ice_power/self.max_ice_power
             el_power = avg_power
 
+        # power share determines the share of the distance driven by ice
         ice_consumption = ice_power_share * distance / 100 * self.ice_consumption_per_100km
+
+        # calculate consumption out of power, consider efficiency
         el_consumption = el_power * delta_t
 
+        # If  consumption exceeds SOC limit, over-amount will be moved to ice
+        if self.SOC - self.SOC_discharge_limit < el_consumption / self.battery_size:
 
-        #If  consumption exceeds SOC limit, overamount will be moved to ice
-        if (self.SOC - self.SOC_limit < el_consumption / self.battery_size):
-            new_el_consumption = (self.SOC - self.SOC_limit) * self.battery_size
-            new_el_power = new_el_consumption / delta_t
+            new_el_consumption = (self.SOC - self.SOC_discharge_limit) * self.battery_size / self.discharge_efficiency
+            new_el_consumption = max(new_el_consumption,0)
+            el_power = new_el_consumption / delta_t * self.discharge_efficiency
             difference = el_consumption - new_el_consumption
+
+            # if soc is not high enough for consumption of timestep, ice_power_share is increased to support el motor
             ice_power_share = ((ice_power+difference/delta_t)/self.max_ice_power)
 
+            # update ice and el consumption with new shares
             ice_consumption = ice_power_share * distance / 100 * self.ice_consumption_per_100km
-            el_consumption = new_el_power * delta_t
+            el_consumption = new_el_consumption
+
+        # store power values in ts
+        self.el_power_ts[time] = el_power
+        self.ice_power_ts[time] = ice_power
+
+        # store consumption in ts
+        self.el_consumption_ts[time] = el_consumption
+        self.ice_consumption_ts[time] = ice_consumption
 
         return el_consumption, ice_consumption
 
-    def drive(self, el_consumption, ice_consumption, distance, delta_t, t):
+    def drive(self, delta_t, time):
 
+        # last consumption entry in consumption ts
+        el_consumption = self.el_consumption_ts[time]
+        ice_consumption = self.ice_consumption_ts[time]
+
+        # discharging of battery
         self.SOC -= el_consumption / self.battery_size
-        self.tank_level -= ice_consumption
-        self.ice_consumption_ts.append(ice_consumption)
-        self.el_consumption_ts.append(el_consumption)
 
-        if(self.recharge and t%self.charging_interval == 0):
-            self.SOC = self.charging_limit
+        # use of fuel
+        self.tank_level -= ice_consumption
+
+        # charging of battery
+        # checks if car has to be charged
+        if self.recharge and time%96 in self.charging_interval and self.charging_limit-self.SOC > 0:
+
+            # checks if battery is going to be fully charged
+            if self.charging_limit-self.SOC < self.c_rate * delta_t * self.charging_efficiency:
+                self.charging_consumption_ts[time] = (
+                        (self.charging_limit-self.SOC)/self.charging_efficiency * self.battery_size)
+                self.SOC = self.charging_limit  # battery gets charged to charging_limit
+
+            else:
+                self.charging_consumption_ts[time] = (
+                        (self.c_rate*delta_t)  * self.battery_size)
+                self.SOC += (self.c_rate*delta_t) * self.charging_efficiency
+                self.SOC = min(self.SOC, self.charging_limit)
+
+        self.SOC_ts[time] = self.SOC
+        self.battery_size = self.battery_size*0.99999
+        self.capacity_ts[time] = self.battery_size
 
         return el_consumption
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from PHEV import Car
 from matplotlib.dates import DateFormatter
+
+
 def read_csv_data(file_path):
     dtype = {
         'ID': str,
@@ -35,11 +37,16 @@ def read_csv_data(file_path):
 
     return driving_profiles
 
+
 def plot_timeseries(start_time, end_time, time_step, data_dict):
+    data_dict = data_dict
     # Create timestamps for 24 hours
     start_time = pd.Timestamp(start_time)  # Start date of your data
     end_time = start_time + pd.Timedelta(hours=end_time)  # End date of your data
     time_index = pd.date_range(start=start_time, end=end_time, freq='15T')
+
+    plot_keys = ["distance_ts", "SOC_ts", "el_consumption_ts", "ice_consumption_ts"]
+    data_dict = {key: data_dict[key] for key in plot_keys if key in data_dict}
 
     units = {
         "distance_ts": "Distance [km]",
@@ -68,7 +75,8 @@ def plot_timeseries(start_time, end_time, time_step, data_dict):
         # Create a DataFrame
         data_df = pd.DataFrame({'Timestamp': time_index[:len(data)], 'Value': data})
         ax.plot(data_df['Timestamp'], data_df['Value'], marker='o', linestyle='-',
-                markersize=3)  # Adjust markersize here
+                markersize=0, linewidth=1)  # Adjust markersize here
+
         ax.set_ylabel(units.get(column))
         ax.set_title(titles.get(column))
         ax.xaxis.set_major_formatter(DateFormatter("%d-%m %H:%M"))
@@ -79,72 +87,80 @@ def plot_timeseries(start_time, end_time, time_step, data_dict):
     plt.show()
 
 
-def run_simulation(configuration):
-
-    t = configuration["start_time"]
-    T = configuration["end_time"]
+def run_simulation(configuration, simulation_year):
+    start_time = configuration["start_time"]
+    end_time = configuration["end_time"]
     delta_t = configuration["delta_t"]
 
-    simulation_results = {
-        "distance_ts": [],
-        "SOC_ts": [],
-        "el_consumption_ts": [],
-        "ice_consumption_ts": []
+    timeframe_start = int((simulation_year - 1) * 8760 * 1 / configuration["delta_t"])
+    timeframe_end = int(simulation_year * 8760 * 1 / configuration["delta_t"])
 
-    }
+    simulation_results = {}
 
     car = configuration["car"]
     ts_consumption = configuration["ts_consumption"]
     ts_distance = configuration["ts_distance"]
 
+    time_step = start_time
+    while time_step < end_time:
+        # Overall simulation time
+        time = int((time_step + (simulation_year - 1) * 8760) * 1 / delta_t)
 
-    while t < T:
-        el_consumption, ice_consumption = (
-            car.calc_consumption(ts_consumption[t*1/delta_t], ts_distance[t*1/delta_t], delta_t))
-        car.drive(el_consumption, ice_consumption, ts_distance[t*1/delta_t], delta_t, t)
-        t = t + delta_t
+        # Calculate consumption with driving profiles
+        car.calc_consumption(ts_consumption[time_step * 1 / delta_t], ts_distance[time_step * 1 / delta_t], delta_t,
+                             time)
 
-        simulation_results["distance_ts"].append(ts_distance[t*1/delta_t])
-        simulation_results["SOC_ts"].append(car.SOC)
+        # Simulate driving with new battery, fuel consumption
+        car.drive(delta_t, time)
 
-    simulation_results["el_consumption_ts"] = car.el_consumption_ts
-    simulation_results["ice_consumption_ts"] = car.ice_consumption_ts
+        time_step = time_step + delta_t
+
+    simulation_results["SOC_ts"] = car.SOC_ts[timeframe_start:timeframe_end]
+    simulation_results["distance_ts"] = ts_distance
+    simulation_results["el_consumption_ts"] = car.el_consumption_ts[timeframe_start:timeframe_end]
+    simulation_results["ice_consumption_ts"] = car.ice_consumption_ts[timeframe_start:timeframe_end]
+    simulation_results["charging_consumption_ts"] = car.charging_consumption_ts[timeframe_start:timeframe_end]
+    simulation_results["battery_capacity"] = car.capacity_ts[timeframe_start:timeframe_end]
 
     return simulation_results
 
-def find_special_days(df):
 
+def find_special_days(df):
     num_bins = 365
     days = []
     for i in range(num_bins):
-        start = i * 24
-        end = (i + 1) * 24
-        bin_data = sum(df['Distance_km'][start:end])
-        if(bin_data>300):
+        start = i * 96
+        end = (i + 1) * 96
+        bin_data = sum(df[start:end])
+        if (bin_data > 300):
             days.append(i)
-
 
     return days
 
 
 def setup(settings):
-
     driving_profiles = read_csv_data(settings["driving_data"])
     df = driving_profiles[settings["ID"]]
     ts_distance = df['Distance_km']
     ts_consumption = df['Consumption_kWh']
 
+    print(find_special_days(df['Distance_km']))
+
     car = Car(
-            battery_size=settings["battery_size"],
-            fuel_tank_size=settings["fuel_tank_size"],
-            SOC=settings["SOC"],
-            ice_consumption_per_100km=settings["ice_consumption_per_100km"],
-            SOC_limit=settings["SOC_limit"],
-            max_el_power=settings["max_el_power"],
-            max_ice_power=settings["max_ice_power"],
-            tank_level=settings["tank_level"],
-            recharge=settings["recharge"],
-            charging_limit=settings["charging_limit"]
+        battery_size=settings["battery_size"],
+        fuel_tank_size=settings["fuel_tank_size"],
+        soc=settings["SOC"],
+        ice_consumption_per_100km=settings["ice_consumption_per_100km"],
+        soc_discharge_limit=settings["SOC_discharge_limit"],
+        max_el_power=settings["max_el_power"],
+        max_ice_power=settings["max_ice_power"],
+        tank_level=settings["tank_level"],
+        recharge=settings["recharge"],
+        charging_limit=settings["charging_limit"],
+        charging_interval=settings["charging_interval"],
+        c_rate=settings["c_rate"],
+        el_charging_efficiency=settings["el_charging_efficiency"],
+        el_discharging_efficiency=settings["el_discharging_efficiency"]
     )
 
     configuration = {
@@ -153,48 +169,187 @@ def setup(settings):
         "car": car,
         "delta_t": settings["delta_t"],
         "end_time": settings["end_time"],
-        "start_time": settings["start_time"]
+        "start_time": settings["start_time"],
+        "cost_parameters": settings["cost_parameters"],
+        "simulation_years": settings["simulation_years"]
     }
 
     return configuration
 
-def main():
 
+def calculate_costs(configuration, ice_consumption, charging_consumption, cost_parameters):
+    discount_rate = 0.01
+    years = 1
+
+    electricity_costs = cost_parameters["electricity_costs"] * sum(charging_consumption)
+    fuel_costs = cost_parameters["fuel_costs"] * sum(ice_consumption)
+    maintenance_costs = cost_parameters["maintenance_costs"] * sum(configuration["ts_distance"])
+
+    running_costs = (
+            fuel_costs + electricity_costs + maintenance_costs
+    )
+
+    fixed_costs = (
+            cost_parameters["vehicle_costs"] - cost_parameters["build_in_battery_costs"] +
+            cost_parameters["cell_amount"] * cost_parameters["battery_cell_price"]
+    )
+
+    annuity_factor = (1 - (1 + discount_rate) ** -years) / discount_rate
+    present_value_running_costs = running_costs * annuity_factor
+    total_present_value = fixed_costs + present_value_running_costs
+
+    print(f"{'Cost_breakdown':<30}{'Euro':>10}")
+    print(f"{'-' * 40}")
+    print(f"{'Running Costs':<30}{running_costs:>10.2f}")
+    print(f"{'Electricity Costs':<30}{cost_parameters["electricity_costs"] * sum(charging_consumption):>10.2f}")
+    print(f"{'Fuel Costs':<30}{cost_parameters["fuel_costs"] * sum(ice_consumption):>10.2f}")
+    print(f"{'Present value running costs':<30}{present_value_running_costs:>10.2f}")
+    print(f"{'Total present value':<30}{total_present_value:>10.2f}")
+    print(f"{'-' * 40}")
+
+    costs = {
+        "running_costs": running_costs,
+        "electricity_costs": electricity_costs,
+        "fuel_costs": fuel_costs,
+        "maintenance_costs": maintenance_costs
+    }
+
+    return costs
+
+
+def print_stats(simulation_results, configuration):
+    print(f"{'Driving_data':<30}{'Value':>10}")
+    print(f"{'-' * 40}")
+    print(f"{'Total distance driven':<30}{sum(simulation_results["distance_ts"]):>10.2f}")
+    print(f"{'Total el consumption':<30}{sum(simulation_results["el_consumption_ts"]):>10.2f}")
+    print(f"{'Total ice consumption':<30}{sum(simulation_results["ice_consumption_ts"]):>10.2f}")
+    print(f"{'Total charging consumption':<30}{(sum(simulation_results["charging_consumption_ts"])):>10.2f}")
+    print(f"{'-' * 40}")
+
+
+def experiment_1():
     settings = {
         "ID": "1.0",
-        "ice_consumption_per_100km": 5,
-        "battery_size": 0.001,
+        "simulation_years": 10,
+        "ice_consumption_per_100km": 10,
+        "battery_size": 20,
         "fuel_tank_size": 50,
-        "SOC": 0.9,
+        "SOC": 0.9,  # starting SOC
+        "el_charging_efficiency": 0.98,
+        "el_discharging_efficiency": 0.9,
         "charging_limit": 0.9,
-        "SOC_limit": 0.2,
+        "charging_interval": [93, 94, 95, 96, 0, 1, 2, 3, 4],
+        "c_rate": 0.5,
+        "SOC_discharge_limit": 0.2,
         "max_el_power": 100,
         "max_ice_power": 100,
         "tank_level": 30,
+        "car_weight": 2000,
+        "battery_weight": 800,
         "driving_data":
             r"C:\Users\alexl\Downloads\emobpy_timeseries_original\emobpy_timeseries_original.csv",
         "delta_t": 0.25,
-        "end_time": 24*120,
-        "start_time": 24*100,
-        "plot": True,
-        "recharge": False
+        "end_time": 24 * 365 - 1,
+        "start_time": 24 * 0,
+        "years": 1,
+        "plot": False,
+        "recharge": True,
+        "calc_econ": True,
+        "cost_parameters": {
+            "vehicle_costs": 30000,  # Costs in Euro
+            "build_in_battery_costs": 8000,
+            "battery_cell_price": 0.5,  # Costs in Euro/Cell
+            "cell_amount": 15000,
+            "fuel_costs": 1.6,  # Costs in Euro/Liter
+            "electricity_costs": 0.4,  # Costs in Euro/kWh
+            "battery_aging_factor": 1.5,
+            "maintenance_costs": 0.0096  # Maintenance costs in euro/km
+        }
+    }
+
+    experiment_results = {
+        "battery_capacity": [],
+        "total_present_value": [],
+        "fuel_consumption": [],
+        "charging_consumption": [],
+        "running_costs": [],
+        "electricity_costs": [],
+        "fuel_costs": [],
+        "maintenance_costs": []
     }
 
     configuration = setup(settings)
 
-    simulation_results = run_simulation(configuration)
+    t = configuration["simulation_years"]
+    for year in range(1, t + 1):
 
-    if(settings["plot"]):
-        plot_timeseries(configuration["start_time"], configuration["end_time"],
-                        configuration["delta_t"], simulation_results)
+        simulation_results = run_simulation(configuration, year)
+
+        print_stats(simulation_results, configuration)
+        costs = calculate_costs(configuration,
+                                simulation_results["ice_consumption_ts"],
+                                simulation_results["charging_consumption_ts"],
+                                configuration["cost_parameters"])
+
+        if settings["plot"]:
+            plot_timeseries(configuration["start_time"], configuration["end_time"],
+                            configuration["delta_t"], simulation_results)
+
+        experiment_results["running_costs"].append(costs["running_costs"])
+        experiment_results["fuel_costs"].append(costs["fuel_costs"])
+        experiment_results["electricity_costs"].append(costs["electricity_costs"])
+        experiment_results["maintenance_costs"].append(costs["maintenance_costs"])
+
+        experiment_results["battery_capacity"].append(np.mean(simulation_results["battery_capacity"]))
+        experiment_results["fuel_consumption"].append(sum(simulation_results["ice_consumption_ts"]))
+        experiment_results["charging_consumption"].append(sum(simulation_results["charging_consumption_ts"]))
+
+    years = range(1, t + 1)
+    # Create a 2x2 multiplot
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+
+    # Plot 1: Cost plot for yearly Total present value
+    axs[0, 0].plot(years, experiment_results["running_costs"], label='El + Fuel Costs', marker='o')
+    axs[0, 0].plot(years, experiment_results["electricity_costs"], marker='o', label='Electricity Costs', color='green')
+    axs[0, 0].plot(years, experiment_results["fuel_costs"], marker='o', label='Fuel Costs', color='orange')
+    axs[0, 0].plot(years, experiment_results["maintenance_costs"], marker='o', label='Maintenance Costs', color='grey')
+    axs[0, 0].set_title('Yearly Running Costs')
+    axs[0, 0].set_xlabel('Year')
+    axs[0, 0].set_ylabel('Running costs (Euro)')
+    axs[0, 0].grid(True)
+    axs[0, 0].legend(loc='upper left')
+
+    # Plot 2: Plot for battery capacity over years
+    axs[0, 1].plot(years, experiment_results["battery_capacity"], marker='o')
+    axs[0, 1].set_title('Battery Capacity Over Years')
+    axs[0, 1].set_xlabel('Year')
+    axs[0, 1].set_ylabel('Battery Capacity (kWh)')
+    axs[0, 1].grid(True)
+
+    # Plot 3: Plot for yearly fuel consumption
+    axs[1, 0].plot(years, experiment_results["fuel_consumption"], marker='o')
+    axs[1, 0].set_title('Yearly Fuel Consumption')
+    axs[1, 0].set_xlabel('Year')
+    axs[1, 0].set_ylabel('Fuel Consumption (liters)')
+    axs[1, 0].grid(True)
+
+    # Plot 4: Plot for yearly electricity consumption
+    axs[1, 1].plot(years, experiment_results["charging_consumption"], marker='o')
+    axs[1, 1].set_title('Yearly Electricity Consumption')
+    axs[1, 1].set_xlabel('Year')
+    axs[1, 1].set_ylabel('Electricity Consumption (kWh)')
+    axs[1, 1].grid(True)
+
+    # Adjust layout
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    experiment_1()
 
     return 0
 
 
-
-
 if __name__ == "__main__":
     main()
-
-
-
